b520d9c1001125fadb06570fab4a9249bd841a4e
github username <github email address>
2020-03-03 16:55:32 -0800

    attribute change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(name: "Jazz", password: "123")
+
+l1 = List.create(name: "Best Action Shows", owner_id: u1)
+
+Vote.create(user_id: u1, list_id: l1)
+
+s1 = Show.create(title: "Who am I?")
+
+ShowsList.create(list_id: l1, show_id: s1)
